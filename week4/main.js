6db2c18b006fdcd247030b5264ba99ab4548d8b2
B4ttl3fox7 <mz2033@hw.ac.uk>
2023-11-03 09:31:19 +0400

    update

---
+++ week4/main.js
@@ -0,0 +1,7 @@
+var myCity = {
+    "name": "Dubai" ,
+    "country": "UAE" ,
+    "place": "Dubai Mall"
+}
+
+console.log(myCity.name)
---
+++ week4/style.cs
@@ -0,0 +1,14 @@
+body, html {
+font-family: 'Helvetica', cursive;
+font-size: 1.09rem;
+color: #23112e;
+}
+html {
+scroll-behavior: smooth;
+}
+header {
+width: 800px;
+display: flex;
+justify-content: space-between;
+align-items: center
+}
