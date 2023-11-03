26a6d1f46a78dfc51042b2cd45bd8d4f57f02e55
B4ttl3fox7 <mz2033@hw.ac.uk>
2023-11-03 09:43:27 +0400

    Update main.js

--- week4/main.js
+++ week4/main.js
@@ -1,12 +1,29 @@
-var myCity = {
+var myCity =[ {
     //Here we are saving the information in a sheet so we type myCity.name.
     "name": "Dubai" ,
     "country": "UAE" ,
     "place": "Dubai Mall"
+},
+
+{
+    "name": "Milan" ,
+    "country": "Italy" ,
+    "place": "Domo di Milan"
+
+},
+
+{
+
+    "name": "Abu Dhabi",
+    "country": "UAE",
+    "place": "Marine Mall"
+
 }
+
+]
 
 console.log(myCity.name);
 
 var myCity2 = ["Abu Dhabi","UAE"];
 //here we are saving the information in an array so we type myCity[1].
-console.log(myCity2[1]);
+console.log(myCity[1].country);
