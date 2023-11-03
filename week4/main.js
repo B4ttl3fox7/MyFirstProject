c7b5a3570b556ce10a01b5accad7680b5119c38a
B4ttl3fox7 <mz2033@hw.ac.uk>
2023-11-03 10:19:58 +0400

    Update main.js

--- week4/main.js
+++ week4/main.js
@@ -1,6 +1,9 @@
 var ourRequest = new XMLHttpRequest();
 ourRequest.open('GET', 'https://z4h33r-lang.github.io/MyFirstProject/week4/cities1.json');
 ourRequest.onload = function() {
-console.log(ourRequest.responseText);
+var ourData = JSON.parse(ourRequest.responseText);
+console.log(ourData[0]);
+
 };
+
 ourRequest.send();
