7eb40089f7f17514986c6405753cdf109864adc8
B4ttl3fox7 <mz2033@hw.ac.uk>
2023-11-03 09:39:00 +0400

    update style

--- week4/main.js
+++ week4/main.js
@@ -1,7 +1,12 @@
 var myCity = {
+    //Here we are saving the information in a sheet so we type myCity.name.
     "name": "Dubai" ,
     "country": "UAE" ,
     "place": "Dubai Mall"
 }
 
-console.log(myCity.name)
+console.log(myCity.name);
+
+var myCity2 = ["Abu Dhabi","UAE"];
+//here we are saving the information in an array so we type myCity[1].
+console.log(myCity2[1]);
