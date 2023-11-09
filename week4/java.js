015129dcd3903045c9825622afc210e6bb08e87a
B4ttl3fox7 <mz2033@hw.ac.uk>
2023-11-09 12:49:38 +0400

    12:49 ( Task Is Done) (Needs Comments)

--- week4/java.js
+++ week4/java.js
@@ -1,20 +1,42 @@
-var weatherContainer = document.getElementById("weather-info");
-var btn = document.getElementById("btn");
-var input = document.getElementById("cityInput");
+var API = '1c3b339e9beefb049032739092fb928e';
 
-btn.addEventListener("click",function()
-{
-    var value = 1;;
-    
-    var ourRequest = new XMLHttpRequest();
-    ourRequest.open('GET', '1c3b339e9beefb049032739092fb928e');
-    
-ourRequest.onload = function() {
-    var ourData = JSON.parse(ourRequest.responseText);
-    renderHTML(ourData);
-    console.log(ourData[0]);
- }
 
- ourRequest.send();
+var Input = document.getElementById('cityInput');
+var btn = document.getElementById('btn');
+var Weather = document.getElementById('weather-info');
+
+
+btn.addEventListener('click', function() {
+   
+    var city = Input.value;
+
+    if (city == null || city == "") {
+        alert('Please enter a city name.');
+        return;
+    }
+
+    
+    var apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API}`;
+
+    fetch(apiUrl)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+           
+            var weatherDetails = `
+                <p>Weather: ${data.weather[0].description}</p>
+                <p>Main Temperature: ${data.main.temp}Celsius</p>
+                <p>Wind Speed: ${data.wind.speed} m/s</p>
+            `;
+            Weather.innerHTML = weatherDetails;
+        })
+        .catch(error => {
+            
+            console.error('Error:', error.message);
+            Weather.innerHTML = '<p>Error fetching weather data. Please try again later.</p>';
+        });
 });
-
