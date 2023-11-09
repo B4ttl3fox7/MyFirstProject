d79511c500930e4116794ad182abf7e5e91aafbe
B4ttl3fox7 <mz2033@hw.ac.uk>
2023-11-09 11:52:56 +0400

    11:52

--- week4/java.js
+++ week4/java.js
@@ -1,2 +1,20 @@
-var ourRequest = new XMLHttpRequest();
-    ourRequest.open('GET', 'https://z4h33r-lang.github.io/MyFirstProject/week4/cities1.json');
+var weatherContainer = document.getElementById("weather-info");
+var btn = document.getElementById("btn");
+var input = document.getElementById("cityInput");
+
+btn.addEventListener("click",function()
+{
+    var value = 
+    
+    var ourRequest = new XMLHttpRequest();
+    ourRequest.open('GET', '1c3b339e9beefb049032739092fb928e');
+    
+ourRequest.onload = function() {
+    var ourData = JSON.parse(ourRequest.responseText);
+    renderHTML(ourData);
+    console.log(ourData[0]);
+ }
+
+ ourRequest.send();
+});
+
